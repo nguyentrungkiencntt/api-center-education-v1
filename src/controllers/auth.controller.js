35ac6c2e802f4a072de4efc7c2d18443b7858604
Thao Le <lehuuthao11@gmail.com>
2026-04-22 09:46:39 +0700

    init project

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -25,5 +25,16 @@ class AuthController {
   }
 }
 
+async function categories(req, res) {
+  try {
+    const responsive = await services.categories(req.body);
+    return res.status(200).json(responsive);
+  } catch (error) {
+    return res.status(500).json({
+      error: 500,
+      message: "Interal Server Error"
+    })
+  }
+}
 
 module.exports = new AuthController();
